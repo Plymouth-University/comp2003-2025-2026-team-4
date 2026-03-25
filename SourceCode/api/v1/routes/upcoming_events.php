079f2e6dec89c3517ea7e63f2fa58e6d452b8153
vesmashana <masa.milicevic@students.plymouth.ac.uk>
2026-03-25 22:31:41 +0000

    update POST

--- SourceCode/api/v1/routes/upcoming_events.php
+++ SourceCode/api/v1/routes/upcoming_events.php
@@ -19,10 +19,11 @@ if ($method === 'GET') {
             event_name AS eventName,
             event_location AS eventLocation,
             event_date AS eventDate,
-            event_time AS eventTime,
+            event_start_time AS eventStartTime,
+            event_end_time AS eventEndTime,
             image_path AS imagePath
         FROM events
-        WHERE CONCAT(event_date," ", event_time) >= NOW()
+        WHERE CONCAT(event_date, " ", event_start_time
         ORDER BY event_date ASC'
     );
     $events = $stmt->fetchAll(PDO::FETCH_ASSOC);
